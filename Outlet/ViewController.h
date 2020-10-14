1f9d257ce79c208fe777159b03dd466bc2010ec4
Vicente Maroto <vicente.maroto@escuelaestech.es>
2020-10-14 18:47:09 +0200

    FIrst Commit

--- Outlet/ViewController.h
+++ Outlet/ViewController.h
@@ -9,6 +9,10 @@
 
 @interface ViewController : UIViewController
 
-
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UILabel *out;
 @end
 
--- Outlet/ViewController.m
+++ Outlet/ViewController.m
@@ -16,7 +16,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.label.text = @"Control Horario";
+    [_button1 setTitle:@"Entrada" forState:UIControlStateNormal];
+    [_button2 setTitle:@"Salida" forState:UIControlStateNormal];
+    UIImage *imagen;
+    imagen= [UIImage imageNamed:@"unnamed"];
+    _imageView.image = imagen;
 }
-
+- (IBAction)btn1Touched:(id)sender {
+    _out.text=@"Has fichado a la entrada";
+}
+- (IBAction)btn2Touched:(id)sender {
+    _out.text=@"Has fichado a la salida";
+}
 
 @end
