652d9940f298a2ea25908291eb09ac2a77869056
Vicente Maroto <vicente.maroto@escuelaestech.es>
2020-10-14 17:44:41 +0200

    Initial Commit

---
+++ Outlet/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Outlet
+//
+//  Created by A4-iMAC01 on 14/10/2020.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
